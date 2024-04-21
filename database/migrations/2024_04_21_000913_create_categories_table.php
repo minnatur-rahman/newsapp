05a9feaee842770d39fb090482e52bb302a22dce
millatur <minnatur7373@gmail.com>
2024-04-21 06:14:55 +0600

    hdsugh

--- database/migrations/2024_04_21_000913_create_categories_table.php
+++ database/migrations/2024_04_21_000913_create_categories_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('category_bn');
+            $table->string('category_en');
+            $table->string('soft_delete')->nullable()->default(0);
             $table->timestamps();
         });
     }
