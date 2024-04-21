b6bc8635ae79a5438ce526c16db90aba1c397100
millatur <minnatur7373@gmail.com>
2024-04-21 22:25:28 +0600

    shdyg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -213,9 +213,7 @@
             <a href="{{ route('home') }}" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-
                 Dashboard
-
               </p>
             </a>
 
