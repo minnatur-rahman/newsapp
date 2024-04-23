de471d161f6179ff1234bcc476acc7543624993f
millatur <minnatur7373@gmail.com>
2024-04-24 05:28:28 +0600

    hsdg

--- resources/views/backend/category/index.blade.php
+++ resources/views/backend/category/index.blade.php
@@ -7,12 +7,14 @@
             <div class="col-sm-6">
                 <h1 class="m-0 text-dark">Categories</h1>
             </div>
-            <div class="col-sm-6">
-
+                                                                                                                              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                    <li class="breadcrumb-item active">Categories</li>
+                </ol>
             </div>
         </div>
     </div>
 </div>
 
 @endsection
-
