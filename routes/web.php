8c0167729a92f883395d0639bebde3b7bcb205a0
millatur <minnatur7373@gmail.com>
2024-04-24 06:11:21 +0600

    shdsyguygs

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('backend.layouts.layout');
 });
 
-Route::get('/home',[HomeController::class, 'Index'])->middleware(['auth', 'admin'])->name('home');
+// Route::get('/home',[HomeController::class, 'Index'])->middleware(['auth', 'admin'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -24,6 +24,15 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'Index'])->middleware(['auth', 'admin']);
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('/admin/dashboard','Index');
+        // Route::get('/admin/login', 'Login');
+    });
 
 Route::get('/admin/categories', [CategoryController::class, 'Index'])->name('categories');
+
+
+
+});
