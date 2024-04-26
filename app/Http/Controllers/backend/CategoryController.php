22179dce35fe9e7e2dca4271ce0289259109c1e5
millatur <minnatur7373@gmail.com>
2024-04-26 18:04:56 +0600

    uhasdhdyg

--- app/Http/Controllers/backend/CategoryController.php
+++ app/Http/Controllers/backend/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,5 +13,13 @@ class CategoryController extends Controller
     {
         $category = DB::table('categories')->get();
         return view('backend.category.index', compact('category'));
+    }
+
+    public function Store(Request $request)
+    {
+        $validated = $request->validate([
+            'category_en' => 'required|unique:categories|max:255',
+            'category_bn' => 'required|unique:categories|max:255',
+        ]);
     }
 }
