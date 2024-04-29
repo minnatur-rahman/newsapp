659b58451775ca109af3dbff3cbff4238a6b1bee
millatur <minnatur7373@gmail.com>
2024-04-29 06:10:31 +0600

    hdysy

--- app/Http/Controllers/backend/CategoryController.php
+++ app/Http/Controllers/backend/CategoryController.php
@@ -3,9 +3,56 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
-    //
+
+    public function Index()
+    {
+       $category = DB::table('categories')->get();
+
+        return view('backend.category.index', compact('category'));
+    }
+
+
+   public function Store (Request $request)
+    {
+
+        $validated = $request->validate([
+            'category_bn' => 'required|unique:categories|max:55',
+            'category_en' => 'required|unique:categories|max:55',
+        ]);
+
+        DB::table('categories')->insert([
+            'category_bn' => $request->category_bn,
+            'category_en' => $request->category_en,
+        ]);
+
+        toastr()->success('Data has been saved successfully!', 'Congrats', ['timeOut' => 5000]);
+       return redirect()->back();
+ }
+
+   public function delete($id)
+   {
+
+
+     // DB::table('categories')->where('id',$id)->delete();
+
+        //  $category = Category::find($id);
+        //  $category->delete();
+
+        Category::destroy($id);
+
+        toastr()->success('Data has been deleted successfully!', 'Congrats', ['timeOut' => 5000]);
+        return redirect()->back();
+   }
+
+
+
+
+
+
 }
