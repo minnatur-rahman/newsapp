2f3bf6ef6997586486de4831702f186b0c1abb9d
millatur <minnatur7373@gmail.com>
2024-05-02 21:09:48 +0600

    hdshd

--- app/Http/Controllers/backend/SubdistrictController.php
+++ app/Http/Controllers/backend/SubdistrictController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subdistrict;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -14,5 +15,32 @@ class SubdistrictController extends Controller
 
         $district = DB::table('districts')->get();
         return view('backend.subdistrict.index',compact('district', 'sub'));
+    }
+
+    public function SubdistrictStore(Request $request)
+    {
+        $validated = $request->validate([
+            'subdistrict_bn' => 'required|unique:subdistricts|max:100',
+            'subdistrict_en' => 'required|unique:subdistricts|max:100',
+            'district_id' => 'required',
+        ]);
+
+        DB::table('subdistricts')->insert([
+            'subdistrict_bn' => $request->subdistrict_bn,
+            'subdistrict_en' => $request->subdistrict_en,
+            'district_id' => $request->district_id,
+        ]);
+
+       toastr()->success('Data has been saved successfully!', 'Congrats', ['timeOut' => 5000]);
+       return redirect()->back();
+    }
+
+    public function Destroy($id)
+    {
+        // Subdistrict::destroy($id);
+        DB::table('subdistricts')->where('id',$id)->delete();
+
+        toastr()->success('Data has been deleted successfully!', 'Congrats', ['timeOut' => 5000]);
+        return redirect()->back();
     }
 }
