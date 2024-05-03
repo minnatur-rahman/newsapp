9d28b6499428078db451da974ff9d004c71f9467
millatur <minnatur7373@gmail.com>
2024-05-03 09:36:54 +0600

    sdgyug

--- resources/views/backend/post/create.blade.php
+++ resources/views/backend/post/create.blade.php
@@ -19,57 +19,112 @@
 
     <section class="content">
         <div class="container-fluid">
-          <div class="row">
-            <!-- left column -->
-            <div class="col-md-12">
-              <!-- general form elements -->
-              <div class="card card-primary">
-                <div class="card-header">
-                  <h3 class="card-title">Post Insert Panel</h3>
-                </div>
-                <!-- /.card-header -->
-                <!-- form start -->
-                <form>
-                  <div class="card-body">
-                    <div class="row">
-                        <div class="form-group col-md-6">
-                            <label for="exampleInputEmail1">Title Bangla</label>
-                            <input type="text" name="title_bn" class="form-control" id="exampleInputEmail1" placeholder="Type Here Bangla">
-                          </div>
-                          <div class="form-group col-md-6">
-                            <label for="exampleInputPassword1">Title English</label>
-                            <input type="text" name="title_en" class="form-control" id="exampleInputPassword1" placeholder="Type Here English">
-                          </div>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputFile">File input</label>
-                      <div class="input-group">
-                        <div class="custom-file">
-                          <input type="file" class="custom-file-input" id="exampleInputFile">
-                          <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+            <div class="row">
+                <!-- left column -->
+                <div class="col-md-12">
+                    <!-- general form elements -->
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Post Insert Panel</h3>
                         </div>
-                        <div class="input-group-append">
-                          <span class="input-group-text">Upload</span>
-                        </div>
-                      </div>
-                    </div>
-                    <div class="form-check">
-                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                    </div>
-                  </div>
-                  <!-- /.card-body -->
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form>
+                            <div class="card-body">
 
-                  <div class="card-footer">
-                    <button type="submit" class="btn btn-warning">Submit</button>
-                  </div>
-                </form>
-              </div>
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputEmail1">Title Bangla</label>
+                                        <input type="text" name="title_bn" class="form-control" id="exampleInputEmail1"
+                                            placeholder="Type Here Bangla">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputPassword1">Title English</label>
+                                        <input type="text" name="title_en" class="form-control"
+                                            id="exampleInputPassword1" placeholder="Type Here English">
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputEmail1">Category</label>
+                                        <select name="cat_id" class="form-control">
+                                            <option selected disabled">==Choose One==</option>
+                                            @foreach ($category as $row)
+                                                <option value="{{ $row->id }}">
+                                                    {{ $row->category_bn }}|{{ $row->category_en }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputPassword1">Subcategory</label>
+                                        <select name="subcat_id" class="form-control">
+                                            <option selected disabled">==Choose One==</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputEmail1">District</label>
+                                        <select name="dist_id" class="form-control">
+                                            <option selected disabled">==Choose One==</option>
+                                            @foreach ($district as $row)
+                                                <option value="{{ $row->id }}">
+                                                    {{ $row->district_bn }}|{{ $row->district_en }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputPassword1">Sub District</label>
+                                        <select name="subdist_id" class="form-control">
+                                            <option selected disabled">==Choose One==</option>
+
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="exampleInputFile">File input</label>
+                                    <div class="input-group">
+                                        <div class="custom-file">
+                                            <input type="file" class="custom-file-input" id="exampleInputFile">
+                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                        </div>
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">Upload</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputEmail1">Tags Bangla</label>
+                                        <input type="text" name="tags_bn" class="form-control" id="exampleInputEmail1"
+                                            placeholder="Type Here Bangla">
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="exampleInputPassword1">Tags English</label>
+                                        <input type="text" name="tags_en" class="form-control"
+                                            id="exampleInputPassword1" placeholder="Type Here English">
+                                    </div>
+                                </div>
+
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-warning">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
- </section>
-
-
-
+    </section>
 @endsection
