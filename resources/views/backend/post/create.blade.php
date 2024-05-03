ca3d5ac558a9a81d269fbf36ddcdbf0b8ac1b021
millatur <minnatur7373@gmail.com>
2024-05-03 06:30:45 +0600

    sdhyudghyg

--- resources/views/backend/post/create.blade.php
+++ resources/views/backend/post/create.blade.php
@@ -4,12 +4,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Categories</h1>
+                    <h1 class="m-0 text-dark">Post Insert Panel</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Categories</li>
+                        <li class="breadcrumb-item active">Post Insert Panel</li>
                     </ol>
                 </div>
             </div>
