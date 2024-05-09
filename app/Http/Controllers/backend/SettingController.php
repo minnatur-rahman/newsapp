b25b120c8d481df5740f81b0101f0c56305aeb92
millatur <minnatur7373@gmail.com>
2024-05-09 21:36:28 +0600

    iahuydg

--- app/Http/Controllers/backend/SettingController.php
+++ app/Http/Controllers/backend/SettingController.php
@@ -11,7 +11,36 @@ class SettingController extends Controller
 {
     public function SocialSetting()
     {
-        $social = DB::table('socials')
-        return view('backend.setting.social');
+        $social = DB::table('socials')->first();
+        return view('backend.setting.social',compact('social'));
+    }
+
+    public function SocialUpdate(Request $request,$id)
+    {
+        $validated = $request->validate([
+            'facebook' => 'required|max:55',
+            'twitter' => 'required|max:55',
+            'youtube' => 'required|max:55',
+            'instagram' => 'required|max:55',
+            'linkedin' => 'required|max:55',
+       ]);
+
+        DB::table('socials')->where('id',$id)->update([
+            'facebook' => $request->facebook,
+            'twitter' => $request->twitter,
+            'youtube' => $request->youtube,
+            'instagram' => $request->instagram,
+            'linkedin' => $request->linkedin,
+
+    ]);
+
+        toastr()->success('Data has been update successfully!', 'Congrats', ['timeOut' => 5000]);
+        return redirect()->back();
+    }
+
+    public function SeoSetting()
+    {
+       $seo = DB::table('seos')->first();
+       return view('backend.setting.seo',compact('seo'));
     }
 }
