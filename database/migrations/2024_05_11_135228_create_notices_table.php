6d22d54ea9cdf6411cd4a6bb6111c4236fc3851e
millatur <minnatur7373@gmail.com>
2024-05-11 19:57:00 +0600

    ahgdsayghdysagh

--- database/migrations/2024_05_11_135228_create_notices_table.php
+++ database/migrations/2024_05_11_135228_create_notices_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('notices', function (Blueprint $table) {
             $table->id();
+            $table->text('notice')->nullable();
+            $table->id('status')->nullable()->default(0);
             $table->timestamps();
         });
     }
