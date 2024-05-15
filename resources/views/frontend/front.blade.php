5406ad2c8f9ac4e02a769c22fd4e0b0c6aad9fa1
millatur <minnatur7373@gmail.com>
2024-05-15 23:31:36 +0600

    ahdysgh

--- resources/views/frontend/front.blade.php
+++ resources/views/frontend/front.blade.php
@@ -108,8 +108,14 @@
                                 </button>
                             </div>
 
-                            <div class="">
+                            <div id="navbarCollapse" class="collapse navbar-collapse">
+                                <ul class="nav navbar-nav">
 
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"></a>
+                                    </li>
+
+                                </ul>
                             </div>
 
                         </nav>
