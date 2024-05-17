d83fad8ce7766fef0d07a9e0c6e63f4fb4e2b162
millatur <minnatur7373@gmail.com>
2024-05-17 18:07:11 +0600

    aiuhsdyug

--- resources/views/frontend/front.blade.php
+++ resources/views/frontend/front.blade.php
@@ -30,6 +30,8 @@
     <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/slick.css">
     <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/nice-select.css">
     <link rel="stylesheet" href="{{ asset('frontend') }}/assets/css/style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 
 <body>
@@ -49,49 +51,21 @@
 
     <nav class="navbar navbar-expand-lg bg-info">
         <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Logo</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
-              </ul>
-              <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-              </form>
-            </div>
+           
+        
         </div>
       </nav>
-
-
-
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        
+    
+           <div>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
                 @foreach ( $category as $cat )
                 <?php
                     $subcategory = DB::table('subcategories')->where('category_id',$cat->id)->get();
@@ -113,6 +87,8 @@
             </ul>
 
           </div>
+           </div>
+
         </div>
       </nav>
       <hr>
@@ -283,6 +259,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+
+
 
 </body>
 
