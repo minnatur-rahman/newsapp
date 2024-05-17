cadaa0607fb75a1bb5921795a3e741070033aed6
millatur <minnatur7373@gmail.com>
2024-05-17 21:17:53 +0600

    akdhsgh

--- resources/views/frontend/front.blade.php
+++ resources/views/frontend/front.blade.php
@@ -54,17 +54,14 @@
             <a class="navbar-brand" href="#">Logo</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
-            </button>     
+            </button>
         </section>
       {{-- </nav> --}}
 
 
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-        
-    
-           <div>
-       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <section class="container navbar navbar-expand-lg bg-info">
+
+       <div class="col-md-8">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 @foreach ( $category as $cat )
                 <?php
@@ -87,10 +84,13 @@
             </ul>
 
           </div>
+          <div>
+
+          </div>
            </div>
 
         </div>
-      </nav>
+      </section>
       <hr>
 
 
