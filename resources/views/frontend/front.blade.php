61b0eeb4cfacab76d4702d948cd6324ae79d8179
millatur <minnatur7373@gmail.com>
2024-05-19 06:25:31 +0600

    ajdhuh

--- resources/views/frontend/front.blade.php
+++ resources/views/frontend/front.blade.php
@@ -1,6 +1,7 @@
 @php
     $category = DB::table('categories')->orderBy('id', 'ASC')->get();
     $seo = DB::table('seos')->first();
+    $social = DB::table('socials')->first();
 @endphp
 
 
@@ -102,9 +103,11 @@
                 <div class="text-center">
                     <ul>
                         @if(session()->get('lang')=='english')
-                        <li class="version"><a href="{{ route('lang.bangla') }}" style="text-decoration: none; text-align: center;">Bangla</a></li>
+                        <li class="version"><a href="{{ route('lang.bangla') }}" style="text-decoration: none; text-align: center;">Bangla</a>
+                        </li>
                         @else
-                        <li class="version"><a href="{{ route('lang.english') }}" style="text-decoration: none; text-align: center;">English</a></li>
+                        <li class="version"><a href="{{ route('lang.english') }}" style="text-decoration: none; text-align: center;">English</a>
+                        </li>
                         @endif
                     </ul>
                 </div>
@@ -121,6 +124,19 @@
                     <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z"/>
                     <path d="M2 3h10v2H2zm0 3h4v3H2zm0 4h4v1H2zm0 2h4v1H2zm5-6h2v1H7zm3 0h2v1h-2zM7 8h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2zm-3 2h2v1H7zm3 0h2v1h-2z"/>
                   </svg>
+            </div>
+
+            <div  class="col-xs-12 col-md-1 col-sm-12">
+                <button class="btn btn-primary dropdown-toggle" type="button" id="socialDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                    Follow Us
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="socialDropdown">
+                    <li><a class="dropdown-item" href="{{ $social->facebook }}" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i>Facebook</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->twitter }}" target="_blank">Twitter</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->instagram }}" target="_blank">Instagram</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->linkedin }}" target="_blank">LinkedIn</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->youtube }}" target="_blank">YouTube</a></li>
+                </ul>
             </div>
           </div>
         </div>
