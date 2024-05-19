d473b43c5e048d1e7dbb80a13cb0a638c1893b0f
millatur <minnatur7373@gmail.com>
2024-05-19 06:44:57 +0600

    iauedygt

--- resources/views/frontend/front.blade.php
+++ resources/views/frontend/front.blade.php
@@ -41,6 +41,9 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -131,11 +134,11 @@
                     Follow Us
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="socialDropdown">
-                    <li><a class="dropdown-item" href="{{ $social->facebook }}" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i>Facebook</a></li>
-                    <li><a class="dropdown-item" href="{{ $social->twitter }}" target="_blank">Twitter</a></li>
-                    <li><a class="dropdown-item" href="{{ $social->instagram }}" target="_blank">Instagram</a></li>
-                    <li><a class="dropdown-item" href="{{ $social->linkedin }}" target="_blank">LinkedIn</a></li>
-                    <li><a class="dropdown-item" href="{{ $social->youtube }}" target="_blank">YouTube</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->facebook }}" target="_blank"><i class="fab fa-facebook-f" aria-hidden="true"></i>Facebook</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->twitter }}" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i>Twitter</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->instagram }}" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i>Instagram</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->linkedin }}" target="_blank"><i class="fab fa-linkedin" aria-hidden="true"></i>LinkedIn</a></li>
+                    <li><a class="dropdown-item" href="{{ $social->youtube }}" target="_blank"><i class="fab fa-youtube" aria-hidden="true"></i>YouTube</a></li>
                 </ul>
             </div>
           </div>
@@ -312,6 +315,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
+
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> --}}
 
 
 
